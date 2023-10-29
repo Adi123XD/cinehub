d4527697c984b0b3e4c877dd2265aeb354f2ceb7
ADI123XD <adarshagarwal932@gmail.com>
2023-10-29 11:58:42 +0530

    fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import { useEffect, useState } from 'react'
 import { fetchDataFromApi } from './utils/api'
+import { useSelector, useDispatch } from 'react-redux'
+import { getApiConfiguration } from './store/homeSlice'
+import './App.css'
+import { BrowserRouter as Router , Routes, Route } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Details from './pages/details/Details'
+import SearchResults from './pages/searchResutls/SearchResults'
+import Explore from './pages/explore/Explore'
+import PageNotFound from './pages/404/PageNotFound'
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 function App() {
+  const dispatch = useDispatch()
+  const url = useSelector((state)=>state.home.url)
   useEffect(()=>{
     apiTesting()
   },[])
@@ -9,11 +22,22 @@ function App() {
     fetchDataFromApi('/discover/movie?')
     .then((res)=>{
       console.log(res)
+      dispatch(getApiConfiguration(res))
     })
   }
 
   return (
-    <div className="App">App</div>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/:mediaType/:id" element={<Details/>}/>
+        <Route path="/search/:query/" element={<SearchResults />}/>
+        <Route path="/explore/:mediaType/" element={<Explore/>}/>
+        <Route path="*" element={<PageNotFound/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
   )
 }
 
---
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.scss'
+const HeroBanner = () => {
+  return (
+    <div>
+      Hero Banner
+    </div>
+  )
+}
+
+export default HeroBanner
